f9ed0577a33a862f1fb9ec2e81fda50e48293d49
qwikster <ocoincs@gmail.com>
2026-07-24 17:09:21 -0400

    feat: install svelte, setup adapter

---
+++ frontend/svelte.config.js
@@ -0,0 +1,18 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+export default {
+  preprocess: vitePreprocess(),
+  compilerOptions: {
+    // Force runes mode for the project, except for libraries. Can be removed in svelte 6.
+		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+  },
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      strict: true
+    })
+  }
+};
