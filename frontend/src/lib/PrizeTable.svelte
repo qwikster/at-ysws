1d76a3a2ba9ea7d34d86dcc05ada82664bacb47b
qwikster <ocoincs@gmail.com>
2026-07-29 01:26:40 -0400

    refactor: componentize YS section table

---
+++ frontend/src/lib/PrizeTable.svelte
@@ -0,0 +1,122 @@
+<script>
+
+</script>
+
+<div class="tiles">
+    <div class="tile">
+        <span class="tile-head">HTTP</span>
+        <div class="tile-body">
+            <div class="tile-part">
+                <div class="tile-l"> YS </div>
+                <div class="tile-r">
+                    <span class="item"><at>@</at> new Requests library</span>
+                    <span class="item"><at>@</at> HTTP client or server</span>
+                    <span class="item"><at>@</at> custom HTTP router</span>
+                </div>
+            </div>
+            <div class="tile-part">
+                <div class="tile-l tlb"> WS </div>
+                <div class="tile-r">
+                    <span class="item"><at>@</at> domain grant [3h]</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="tile">
+        <span class="tile-head">TCP/IP</span>
+        <div class="tile-body">
+            <div class="tile-part">
+                <div class="tile-l"> YS </div>
+                <div class="tile-r">
+                    <span class="item"><at>@</at> JSON chat app</span>
+                    <span class="item"><at>@</at> new protocol atop TCP</span>
+                </div>
+            </div>
+            <div class="tile-part">
+                <div class="tile-l tlb"> WS </div>
+                <div class="tile-r">
+                    <span class="item"><at>@</at> Ethernet patch cables [2h]</span>
+                    <span class="item"><at>@</at> Raspberry Pi Zero 2 W [10h]</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .tiles {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        gap: var(--gap-l);
+        margin-top: var(--pad-v);
+    }
+
+    .tile {
+        display: flex;
+        flex-grow: 1;
+        white-space: nowrap;
+        flex-direction: column;
+        border: var(--border-sm) solid var(--ac);
+        border-radius: 1rem;
+    }
+
+    .tile-head {
+        text-align: center;
+        font-weight: 800;
+        font-size: var(--font-header);
+
+        border-radius: 0.8rem 0.8rem 0px 0px;
+        border-bottom: 0.2rem dashed var(--fg);
+
+        background: repeating-linear-gradient(
+          -45deg,
+          var(--dim),
+          var(--dim) 10px,
+          var(--dimmer) 10px,
+          var(--dimmer) 20px
+        );
+    }
+
+    .tile-body {
+        font-size: var(--font-body);
+        display: flex;
+        flex-direction: column;
+    }
+
+    .tile-part {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .tile-part:first-child {
+        border-bottom: 0.2rem dashed var(--fg);
+    }
+
+    .tile-l {
+        display: flex;
+        align-items: center;
+        height: 100%;
+        border-right: 0.2rem dashed var(--fg);
+        padding: 0rem 0.3rem 0rem 0.2rem;
+        font-weight: 500;
+        color: var(--sc);
+        text-shadow: 1px 1px 2px var(--sc);
+        background-color: rgb(from var(--dim) r g b / 50%);
+    }
+
+    .tlb {
+        border-bottom-left-radius: 0.8rem;
+    }
+
+    .tile-r {
+        padding: 0.2rem 0rem 0.2rem 0.4rem;
+    }
+
+    .item {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 0.4rem;
+    }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import hc from "$lib/assets/hc.svg"
+    import PrizeTable from "$lib/PrizeTable.svelte";
 
     /* let dmtxt = $state("dark mode")
     function darkmode() {
@@ -71,11 +72,11 @@
     </div>
     <div class="callout">
         <div class="track">
-        <div class="prize">
-            <div class="prize-art">
+            <div class="prize">
+                <div class="prize-art">
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;########
        ##------##
- #######-.##+-.-#######
+#######-.##+-.-#######
 ###---##-.####.-##---###
 #++#+--##+----+##--+#++#
 #+--+#+-+######+-+#+--+#
@@ -84,10 +85,13 @@
 #+-+##+--++##++--+##+-+#
 ####+--------------+####
 ########################
+                </div>
+                <div class="prize-name">
+                    1yr Purelymail
+                </div>
             </div>
-            <div class="prize-name">
-                1yr Purelymail
-            </div>
+        <div class="prize">
+            meow
         </div>
         </div>
     </div>
@@ -95,49 +99,7 @@
         <h1>1. You Ship</h1>
         <p>Pick a protocol, or choose your own:</p>
 
-        <div class="tiles">
-            <div class="tile">
-                <span class="tile-head">HTTP</span>
-                <div class="tile-body">
-                    <div class="tile-part">
-                        <div class="tile-l"> YS </div>
-                        <div class="tile-r">
-                            <span class="item"><at>@</at> new Requests library</span>
-                            <span class="item"><at>@</at> HTTP client or server</span>
-                            <span class="item"><at>@</at> custom HTTP router</span>
-                        </div>
-                    </div>
-                    <div class="tile-part">
-                        <div class="tile-l tlb"> WS </div>
-                        <div class="tile-r">
-                            <span class="item"><at>@</at> domain grant [3h]</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tile">
-                <span class="tile-head">TCP/IP</span>
-                <div class="tile-body">
-                    <div class="tile-part">
-                        <div class="tile-l"> YS </div>
-                        <div class="tile-r">
-                            <span class="item"><at>@</at> JSON chat app</span>
-                            <span class="item"><at>@</at> new protocol atop TCP</span>
-                        </div>
-                    </div>
-                    <div class="tile-part">
-                        <div class="tile-l tlb"> WS </div>
-                        <div class="tile-r">
-                            <span class="item"><at>@</at> Ethernet patch cables [2h]</span>
-                            <span class="item"><at>@</at> Raspberry Pi Zero 2 W [10h]</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="tile">:woomy:</div>
-            <div class="tile">:woomy: but extra LONG</div>
-            <div class="tile">:woomy: but it's EVEN LONGER and on a DIFFERENT LINE</div>
-        </div>
+        <PrizeTable/>
     </div>
 
 </div>
@@ -256,81 +218,6 @@
         user-select: text;
     }
 
-    .tiles {
-        display: flex;
-        flex-wrap: wrap;
-        width: 100%;
-        gap: var(--gap-l);
-        margin-top: var(--pad-v);
-    }
-
-    .tile {
-        display: flex;
-        flex-grow: 1;
-        white-space: nowrap;
-        flex-direction: column;
-        border: var(--border-sm) solid var(--ac);
-        border-radius: 1rem;
-    }
-
-    .tile-head {
-        text-align: center;
-        font-weight: 800;
-        font-size: var(--font-header);
-
-        border-radius: 0.8rem 0.8rem 0px 0px;
-        border-bottom: 0.2rem dashed var(--fg);
-
-        background: repeating-linear-gradient(
-          -45deg,
-          var(--dim),
-          var(--dim) 10px,
-          var(--dimmer) 10px,
-          var(--dimmer) 20px
-        );
-    }
-
-    .tile-body {
-        font-size: var(--font-body);
-        display: flex;
-        flex-direction: column;
-    }
-
-    .tile-part {
-        display: flex;
-        flex-direction: row;
-    }
-
-    .tile-part:first-child {
-        border-bottom: 0.2rem dashed var(--fg);
-    }
-
-    .tile-l {
-        display: flex;
-        align-items: center;
-        height: 100%;
-        border-right: 0.2rem dashed var(--fg);
-        padding: 0rem 0.3rem 0rem 0.2rem;
-        font-weight: 500;
-        color: var(--sc);
-        text-shadow: 1px 1px 2px var(--sc);
-        background-color: rgb(from var(--dim) r g b / 50%);
-    }
-
-    .tlb {
-        border-bottom-left-radius: 0.8rem;
-    }
-
-    .tile-r {
-        padding: 0.2rem 0rem 0.2rem 0.4rem;
-    }
-
-    .item {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        gap: 0.4rem;
-    }
 
     @keyframes slideInTop {
         from {
